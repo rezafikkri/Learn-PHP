39d494c65c352de576c73a84ef9cac2c864c7845
rezafikkri <fikkri.reza@gmail.com>
2023-06-05 06:42:20 +0700

    refactor: add info for code result in OperatorArray.php

--- php dasar/OperatorArray.php
+++ php dasar/OperatorArray.php
@@ -6,8 +6,8 @@ var_dump($a + $b); // Union
 
 $a = ["Dea", "Dian"];
 $b = [1 => "Dian", 0 => "Dea"];
-var_dump($a == $b); // Equality
-var_dump($a === $b); // Identity
+var_dump($a == $b); // Equality (true)
+var_dump($a === $b); // Identity (false)
 
 var_dump($a != $b); // inEquality
 var_dump($a !== $b); // inIdentity
