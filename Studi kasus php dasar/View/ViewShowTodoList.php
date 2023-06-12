5006ffd372d4d8d52531bdf0c2b86e851e1337cc
rezafikkri <fikkri.reza@gmail.com>
2023-06-12 17:01:56 +0700

    feat: View show todolist

--- Studi kasus php dasar/View/ViewShowTodoList.php
+++ Studi kasus php dasar/View/ViewShowTodoList.php
@@ -1,6 +1,33 @@
 <?php
 
+require_once "BusinessLogic/ShowTodoList.php";
+require_once "BusinessLogic/AddTodoList.php";
+require_once "BusinessLogic/RemoveTodoList.php";
+require_once "Helper/Input.php";
+
 function viewShowTodoList()
 {
+    while (true) {
+        showTodoList();
 
+        echo "MENU" . PHP_EOL;
+        echo "1. Tambah Todo" . PHP_EOL;
+        echo "2. Hapus Todo" . PHP_EOL;
+        echo "x. Keluar" . PHP_EOL;
+
+        $pilihan = input("Pilih");
+
+        if ($pilihan == 1) {
+            viewAddTodoList();
+        } elseif ($pilihan == 2) {
+            viewRemoveTodoList();
+        } elseif ($pilihan == 'x') {
+            // Keluar
+            break;
+        } else {
+            echo "Pilihan tidak dimengerti" . PHP_EOL;
+        }
+    }
+
+    echo "Sampai jumpa lagi";
 }
