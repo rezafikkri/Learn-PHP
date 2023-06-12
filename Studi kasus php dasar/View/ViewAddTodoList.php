43ae9c7c8292f0cfce4493f3af48cf0c1c32269b
rezafikkri <fikkri.reza@gmail.com>
2023-06-13 06:02:52 +0700

    feat: View Add Todo list

--- Studi kasus php dasar/View/ViewAddTodoList.php
+++ Studi kasus php dasar/View/ViewAddTodoList.php
@@ -1,6 +1,18 @@
 <?php
 
+require_once "Model/TodoList.php";
+require_once "Helper/Input.php";
+require_once "BusinessLogic/AddTodoList.php";
+
 function viewAddTodoList()
 {
+    echo "Menambah Todo" . PHP_EOL;
 
+    $todo = input("Todo (x untuk batal)");
+
+    if ($todo == 'x') {
+        // batal
+    } else {
+        addTodoList($todo);
+    }
 }
