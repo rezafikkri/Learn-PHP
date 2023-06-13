b3b05a4843e44478e618e27b50c79fcb4918c236
rezafikkri <fikkri.reza@gmail.com>
2023-06-13 11:42:49 +0700

    feat: View remove todolist

--- Studi kasus php dasar/View/ViewRemoveTodoList.php
+++ Studi kasus php dasar/View/ViewRemoveTodoList.php
@@ -1,6 +1,23 @@
 <?php
 
+require_once "Model/TodoList.php";
+require_once "Helper/Input.php";
+require_once "BusinessLogic/RemoveTodoList.php";
+
 function viewRemoveTodoList()
 {
+    echo "MENGHAPUS TODO" . PHP_EOL;
 
+    $pilihan = input("Nomor (x untuk batalkan)");
+
+    if ($pilihan == 'x') {
+        echo "Batal menghapus todo";
+    } else {
+        $success = removeTodoList($pilihan);
+        if ($success) {
+            echo "Success Menghapus Todo Nomor $pilihan";
+        } else {
+            echo "Success Menghapus Todo Nomor $pilihan";
+        }
+    }
 }
