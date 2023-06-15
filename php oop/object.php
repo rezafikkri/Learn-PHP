aac816474c38fbfaae5cf3a1ec6231401e208a62
rezafikkri <fikkri.reza@gmail.com>
2023-06-15 17:45:50 +0700

    refactor: object.php and properties.php

--- php oop/object.php
+++ php oop/object.php
@@ -5,6 +5,8 @@ require_once "properties.php";
 $person = new \properties\Person();
 $person->name = "Reza";
 
-var_dump($person);
+/* var_dump($person);
 
-echo "Nama : $person->name";
+echo "Nama : $person->name"; */
+
+$person->sayHello();
--- php oop/properties.php
+++ php oop/properties.php
@@ -7,11 +7,13 @@ class Person
     var string $name;
     var ?string $address = null;
     var string $country = "Indonesia";
-}
 
-class Programmer
-{
-    public function FunctionName($var = null) {
-        echo 'reza';
+    public function sayHello(?string $name = null)
+    {
+        if (is_null($name)) {
+            echo "Hello, my name is $this->name" . PHP_EOL;
+        } else {
+            echo "Hello $name, my name is $this->name" . PHP_EOL;
+        }
     }
 }
