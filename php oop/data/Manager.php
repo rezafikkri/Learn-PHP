d6593fa94d528d5b5008b08cd8d274c56994f5d6
rezafikkri <fikkri.reza@gmail.com>
2023-06-22 18:00:19 +0700

    feat: constructor overidding

--- php oop/data/Manager.php
+++ php oop/data/Manager.php
@@ -4,20 +4,26 @@ class Manager
 {
     public function __construct(
         protected string $name,
+        public string $title = "Manager",
     ) {
         
     }
 
     public function sayHello(string $name): void
     {
-        echo "Hi $name, my name is Manager $this->name" . PHP_EOL;
+        echo "Hi $name, my name is $this->title $this->name" . PHP_EOL;
     }
 }
 
 class VicePresident extends Manager
 {
+    public function __construct(string $name = "")
+    {
+        parent::__construct($name, "VP");
+    }
+
     public function sayHello(string $name): void
     {
-        echo "Hi $name, my name is VP $this->name" . PHP_EOL;
+        echo "Hi $name, my name is $this->title $this->name" . PHP_EOL;
     }
 }
