d081c3b5cef217423db878729fc26effd70fb19c
rezafikkri <fikkri.reza@gmail.com>
2023-06-26 08:55:44 +0700

    feat: final function

--- php oop/data/SocialMedia.php
+++ php oop/data/SocialMedia.php
@@ -7,12 +7,18 @@ class SocialMedia
     public string $name;
 }
 
-final class Facebook extends SocialMedia
+class Facebook extends SocialMedia
 {
-
+    final public function login(string $username, stirng $password): bool
+    {
+        return true;
+    }
 }
 
 class FakeFacebook extends Facebook
 {
-
+    public function login(string $username, stirng $password): bool
+    {
+        return false;
+    }
 }
