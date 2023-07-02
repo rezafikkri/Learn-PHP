df5fe8cb46b1a01471d845bb9e94fc6c297daf18
rezafikkri <fikkri.reza@gmail.com>
2023-07-02 09:41:50 +0700

    feat: Contravariance

--- php oop/Covariance.php
+++ php oop/Covariance.php
@@ -1,13 +1,15 @@
 <?php
 
-require_once "data/AnimalShelter.php";
+use Data\{AnimalFood, CatShelter, DogShelter, Food};
 
-use Data\{CatShelter, DogShelter};
+require_once "data/AnimalShelter.php";
 
 $catShelter = new CatShelter();
 $cat = $catShelter->adopt("Kureng");
 var_dump($cat);
+$cat->eat(new AnimalFood());
 
 $dogShelter = new DogShelter();
 $dog = $dogShelter->adopt("Haha");
 var_dump($dog);
+$dog->eat(new Food());
