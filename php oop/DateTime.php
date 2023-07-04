7e1a7f244d4b2628317ba6ca5640e1dc8183da16
rezafikkri <fikkri.reza@gmail.com>
2023-07-04 09:37:00 +0700

    feat: date interval

--- php oop/DateTime.php
+++ php oop/DateTime.php
@@ -3,3 +3,11 @@
 $dateTime = new DateTime();
 $dateTime->setDate(2001, 01, 10)->setTime(10, 10, 10, 0);
 var_dump($dateTime);
+
+$dateTime->add(new DateInterval("P1Y"));
+var_dump($dateTime);
+
+$minusOneMonth = new DateInterval("P1M");
+$minusOneMonth->invert = 1;
+$dateTime->add($minusOneMonth);
+var_dump($dateTime);
