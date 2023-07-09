419751732b089c4d2b79615391467e815efee01d
rezafikkri <fikkri.reza@gmail.com>
2023-07-09 08:52:47 +0700

    feat: Union types

---
+++ php 8/UnionTypes.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace PHP8;
+
+class Example
+{
+    public string $data;
+}
+
+$example = new Example;
+$example->data = 12;
+$example->data = false;
+
+function sample(string|array|bool $data): string|array|false
+{
+    if (is_string($data)) {
+        return "string";
+    } elseif (is_array($data)) {
+        return ["array"];
+    } elseif (is_bool($data)) {
+        return false;
+    }
+}
+
+var_dump(sample(12));
+var_dump(sample([]));
+var_dump(sample(false));
