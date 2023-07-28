af312c9a3dd9f57d9c05a7ddb7b9ba09435af3a9
rezafikkri <fikkri.reza@gmail.com>
2023-07-28 19:20:55 +0700

    feat: Function Overriding Class

---
+++ php 8/FunctionOverridingClass.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace PHP8;
+
+class ParentClass
+{
+    private function method(string $name)
+    {
+
+    }
+}
+
+class ChildClass extends ParentClass
+{
+    public function method(int $name)
+    {
+        
+    }
+}
