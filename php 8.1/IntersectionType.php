7373297814a73c92a16151f1c297f0969ad0918c
rezafikkri <fikkri.reza@gmail.com>
2023-08-07 09:02:09 +0700

    feat: Intersection type

---
+++ php 8.1/IntersectionType.php
@@ -0,0 +1,38 @@
+<?php
+
+interface HasBrand
+{
+    public function getBrand(): string;
+}
+
+interface HasName
+{
+    public function getName(): string;
+}
+
+function printBrandAndName(HasBrand&HasName $value)
+{
+    echo $value->getBrand() . "-" . $value->getName() . PHP_EOL;
+}
+
+class Car implements HasBrand, HasName
+{
+    public function __construct(
+        public string $brand,
+        public string $name,
+    ) {
+
+    }
+
+    public function getBrand(): string
+    {
+        return $this->brand;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
+
+printBrandAndName(new Car("Mitsubitshi", "Lancer"));
