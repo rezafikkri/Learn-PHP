3e989fa401787b71f8a7ed8e35b94e5beba552d1
rezafikkri <fikkri.reza@gmail.com>
2023-08-11 10:52:05 +0700

    feat: Entity and Repository

---
+++ Studi kasus php dasar/Entity/TodoList.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Entity;
+
+class TodoList
+{
+    public function __construct(
+        private string $todo = "",
+    ) {
+    }
+
+    public function getTodo(): string
+    {
+        return $this->todo;
+    }
+
+    public function setTodo(string $todo): void
+    {
+        $this->todo = $todo;
+    }
+}
