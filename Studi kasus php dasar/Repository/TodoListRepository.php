ffa3d2b07aa5b690c380620448de14e1512ee12e
rezafikkri <fikkri.reza@gmail.com>
2023-08-12 13:20:24 +0700

    refactor: finish todo list repository

--- Studi kasus php dasar/Repository/TodoListRepository.php
+++ Studi kasus php dasar/Repository/TodoListRepository.php
@@ -2,4 +2,11 @@
 
 namespace Repository;
 
-class 
+use Entity\TodoList;
+
+interface TodoListRepository
+{
+    public function save(TodoList $todoList): void;
+    public function remove(int $number): bool;
+    public function getAll(): array;
+}
