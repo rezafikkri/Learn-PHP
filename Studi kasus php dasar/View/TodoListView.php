5adcd861f65a25de7382b41c79e4603e5e5c7fac
rezafikkri <fikkri.reza@gmail.com>
2023-08-13 06:57:26 +0700

    feat: Todo list view

---
+++ Studi kasus php dasar/View/TodoListView.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace View;
+
+class TooListView
+{
+    public function showTodoList(): void
+    {
+
+    }
+
+    public function addTodoList(): void
+    {
+
+    }
+
+    public function removeTodoList(): void
+    {
+
+    }
+}
