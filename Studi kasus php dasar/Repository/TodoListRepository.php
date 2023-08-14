780976ca2fe48249b1a75b676180b3a51d7f8e22
rezafikkri <fikkri.reza@gmail.com>
2023-08-14 07:41:56 +0700

    refactor: implement service and repository

--- Studi kasus php dasar/Repository/TodoListRepository.php
+++ Studi kasus php dasar/Repository/TodoListRepository.php
@@ -10,3 +10,23 @@ interface TodoListRepository
     public function remove(int $number): bool;
     public function getAll(): array;
 }
+
+class TodoListRepositoryImpl implements TodoListRepository
+{
+    private array $todoList = [];
+
+    public function save(TodoList $todoList): void
+    {
+        
+    }
+
+    public function remove(int $number): bool
+    {
+        
+    }
+
+    public function getAll(): array
+    {
+        return $this->todoList;
+    }
+}
--- Studi kasus php dasar/Service/TodoListService.php
+++ Studi kasus php dasar/Service/TodoListService.php
@@ -2,11 +2,38 @@
 
 namespace Service;
 
+use Repository\TodoListRepository;
+
 interface TodoListService
 {
     public function showTodoList(): void;
-
     public function addTodoList(string $todo): void;
-
     public function removeTodoList(int $number): void;
+}
+
+class TodoListServiceImpl implements TodoListService
+{
+    public function __construct(
+        private TodoListRepository $todoListRepository,
+    ) {
+    }
+
+    public function showTodoList(): void
+    {
+        
+    }
+
+    public function addTodoList(string $todo): void
+    {
+        
+    }
+
+    public function removeTodoList(int $number): void
+    {
+        echo "TODOLIST" . PHP_EOL;
+
+        foreach ($this->todoListRepository->getAll() as $key => $val) {
+            echo "$key. $val" . PHP_EOL;
+        }
+    }
 }
