37187a5c280fc503e7e5ea380f7469ecad3b1e89
rezafikkri <fikkri.reza@gmail.com>
2023-08-19 15:18:42 +0700

    feat: finish todo list

--- Studi kasus php dasar/App.php
+++ Studi kasus php dasar/App.php
@@ -1,9 +1,17 @@
 <?php
 
-require_once __DIR__ . "/Model/TodoList.php";
-require_once __DIR__ . "/Helper/Input.php";
-require_once __DIR__ . "/View/ViewShowTodoList.php";
+require_once __DIR__ . "/Repository/TodoListRepository.php";
+require_once __DIR__ . "/Service/TodoListService.php";
+require_once __DIR__ . "/View/TodoListView.php";
+
+use Repository\TodoListRepositoryImpl;
+use Service\TodoListServiceImpl;
+use View\TodoListView;
 
 echo "Aplikasi todolist" . PHP_EOL;
 
-viewShowTodoList();
+$todoListRepository = new TodoListRepositoryImpl();
+$todoListService = new TodoListServiceImpl($todoListRepository);
+$todoListView = new TodoListView($todoListService);
+
+$todoListView->showTodoList();
--- Studi kasus php dasar/Test/TestInput.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . "/../Helper/Input.php";
-
-$name = input("Name");
-echo "Hello $name" . PHP_EOL;
-
-$channel = input("Channel");
-echo $channel . PHP_EOL;
