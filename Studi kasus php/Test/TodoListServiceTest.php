0523100e7997d45a11ed1930020edce8c0511a15
rezafikkri <fikkri.reza@gmail.com>
2023-12-28 12:02:13 +0700

    refactor: test add todo to db

--- Studi kasus php/Test/TodoListServiceTest.php
+++ Studi kasus php/Test/TodoListServiceTest.php
@@ -2,7 +2,9 @@
 
 require_once __DIR__ . "/../Repository/TodoListRepository.php";
 require_once __DIR__ . "/../Service/TodoListService.php";
+require_once __DIR__ . "/../Config/Database.php";
 
+use Config\Database;
 use Entity\TodoList;
 use Repository\TodoListRepositoryImpl;
 use Service\TodoListServiceImpl;
@@ -21,14 +23,17 @@ function testShowTodoList(): void
 
 function testAddTodoList(): void
 {
-    $todoListRepository = new TodoListRepositoryImpl();
+    $dbh = Database::getConnection();
+    $todoListRepository = new TodoListRepositoryImpl($dbh);
 
     $todoListService = new TodoListServiceImpl($todoListRepository);
     $todoListService->addTodoList("Belajar PHP");
     $todoListService->addTodoList("Belajar PHP OOP");
     $todoListService->addTodoList("Belajar PHP Database");
     
-    $todoListService->showTodoList();
+    //$todoListService->showTodoList();
+
+    $dbh = null;
 }
 
 function testRemoveTodoList(): void
@@ -52,4 +57,4 @@ function testRemoveTodoList(): void
     $todoListService->showTodoList();
 }
 
-testRemoveTodoList();
+testAddTodoList();
