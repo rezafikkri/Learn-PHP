60046a8d5461fc9dea855ac5d78749a83480d23f
rezafikkri <fikkri.reza@gmail.com>
2023-12-31 08:39:54 +0700

    feat: response code

---
+++ PHP-Web/response-code.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!isset($_GET['name']) || empty($_GET['name'])) {
+    http_response_code(400);
+    echo "Name is Required";
+    exit;
+}
+
+$say = "Hello " . htmlspecialchars($_GET['name']);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1><?= $say ?></h1> 
+    </body>
+</html>
