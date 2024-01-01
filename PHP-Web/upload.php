42915ed0eb824fb3c97d1021d4de80e43243ae5a
rezafikkri <fikkri.reza@gmail.com>
2024-01-01 19:37:16 +0700

    feat: upload file

---
+++ PHP-Web/upload.php
@@ -0,0 +1,34 @@
+<?php
+
+if ($_FILES['image']['error'] !== 0) {
+    die("Upload gagal");
+}
+
+$filepath = $_FILES['image']['tmp_name'];
+$fileSize = filesize($filepath);
+$finfo = new finfo();
+$filetype = $finfo->file($filepath, FILEINFO_MIME_TYPE);
+
+$allowedTypes = [
+    'image/jpeg' => 'jpg',
+    'image/png' => 'png'
+];
+
+// check error
+if ($fileSize > (1024 * 1024)) {
+    die("Ukuran file tidak bisa melebihi 1MB");
+}
+if (!in_array($filetype, array_keys($allowedTypes))) {
+    die("Type file harus png atau jpg");
+}
+
+$filename = bin2hex(random_bytes(10));
+$extention = $allowedTypes[$filetype];
+$dir = __DIR__ . "/image/";
+$newFilePath = $dir . $filename .'.'. $extention;
+
+if (move_uploaded_file($filepath, $newFilePath)) {
+    echo "Upload berhasil";
+} else {
+    echo "Upload gagal";
+}
