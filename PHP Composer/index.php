2167ca912b3d0a4048d3dffc5030bf32bea6405b
rezafikkri <fikkri.reza@gmail.com>
2024-01-05 09:53:59 +0700

    feat: remove old php composer folder

--- PHP Composer/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . "/vendor/autoload.php";
-
-use PhpComposer\Data\Programmer;
-
-$programmer = new Programmer("Reza", "Web Developer");
-
-echo $programmer->sayHello("Dian") . " saya seorang " . $programmer->job . PHP_EOL;
