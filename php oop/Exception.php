d4ff827654f93d1487ca8ef9e422c4b9d35257f7
rezafikkri <fikkri.reza@gmail.com>
2024-07-21 12:03:18 +0700

    feat: exception

--- php oop/Exception.php
+++ php oop/Exception.php
@@ -6,17 +6,29 @@ require_once "helper/Validation.php";
 
 $loginRequest = new LoginRequest();
 $loginRequest->username = "reza";
-$loginRequest->password = "";
+$loginRequest->password = " ";
 
-try {
-    validateLoginRequest($loginRequest);
-    echo "Valid" . PHP_EOL;
-} catch (Exception | ValidationException $e) {
-    echo "Error : {$e->getMessage()}" . PHP_EOL;
+// try {
+//     validateLoginRequest($loginRequest);
+//     echo "Valid" . PHP_EOL;
+// } catch (ValidationException | Exception $e) {
+//     echo "Error: {$e->getMessage()}" . PHP_EOL;
+//     // echo "Error : {$e->getMessage()}" . PHP_EOL;
+//
+//     var_dump($e->getTrace());
+//
+//     // echo $e->getTraceAsString() . PHP_EOL;
+// } finally {
+//     echo "Tetap akan berjalan" . PHP_EOL;
+// }
 
+// Using exception handler
+function exception_handler(ValidationException | Exception $e) {
+    echo "Error: {$e->getMessage()}" . PHP_EOL;
     var_dump($e->getTrace());
-
-    echo $e->getTraceAsString() . PHP_EOL;
-} finally {
-    echo "Tetap akan berjalan" . PHP_EOL;
+    echo $e->getTraceAsString();
 }
+
+set_exception_handler('exception_handler');
+
+validateLoginRequest($loginRequest);
