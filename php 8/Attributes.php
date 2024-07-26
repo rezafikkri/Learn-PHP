6f45cd407ae26c7af1a828b33cb2cb6a69171f8f
rezafikkri <fikkri.reza@gmail.com>
2024-07-26 17:43:04 +0700

    feat: Attributes

--- php 8/Attributes.php
+++ php 8/Attributes.php
@@ -7,9 +7,7 @@ use Exception;
 use ReflectionClass;
 use ReflectionProperty;
 
-// ketika mendeklarasikan attribute tidak sesuai target
-// maka akan throw exception ketika menjalankan ReflectionAttribute::newInstance()
-#[Attribute(Attribute::TARGET_CLASS)] 
+#[Attribute(Attribute::TARGET_PROPERTY)] // will error when call ReflectionAttribute::newInstance()
 class NotBlank
 {
 
@@ -22,64 +20,64 @@ class Length
         public int $min,
         public int $max,
     ) {
+        
     }
 }
 
 class LoginRequest
 {
-    #[Length(min: 4, max: 10)]
     #[NotBlank]
-    public string $username;
-    
-    #[NotBlank, Length(min: 8, max: 10)]
-    public string $password;
+    #[Length(min: 4, max: 5)]
+    public ?string $username;
 
-    public string $age;
+    #[NotBlank]
+    #[Length(max: 5, min: 4)]
+    public ?string $password;
 }
 
 function validate(object $object): void
 {
-    $reflection = new ReflectionClass($object);
-    $properties = $reflection->getProperties();
-
+    $class = new ReflectionClass($object);
+    $properties = $class->getProperties();
     foreach ($properties as $property) {
-        validateNotBlank($property, $object);
-        validateLength($property, $object);
-    }
-}
-
-function validateNotBlank(ReflectionProperty $property, object $object): void
-{
-    $attributes = $property->getAttributes(NotBlank::class);
-    if (count($attributes) > 0) {
-        if (!$property->isInitialized($object)) {
-            throw new Exception("Property $property->name is not set");
-        } elseif (trim($property->getValue($object)) == "") {
-            throw new Exception("Property $property->name is empty");
-        }
-    }
-}
-
-function validateLength(ReflectionProperty $property, object $object): void
-{
-    if (!$property->isInitialized($object)) return;
-
-    $attributes = $property->getAttributes(Length::class);
-    if (count($attributes) > 0) {
-        $valueLength = strlen($property->getValue($object));
-
-        foreach ($attributes as $a) {
-            $length = $a->newInstance();
-            if ($valueLength < $length->min) {
-                throw new Exception("Property $property->name is too short");
-            } elseif ($valueLength > $length->max) {
-                throw new Exception("Property $property->name is too long");
+        $attributes = $property->getAttributes();
+        foreach ($attributes as $attribute) {
+            switch ($attribute->getName()) {
+                case NotBlank::class:
+                    validateNotBlank($property, $object);
+                    break;
+                case Length::class:
+                    validateLength($property, $object, $attribute->newInstance());
+                    break;
             }
         }
     }
 }
 
-$request = new LoginRequest();
-$request->username = "reza";
-$request->password = "reeza1234";
-validate($request);
+function validateNotBlank(ReflectionProperty $property, object $object): void
+{
+    if (!$property->isInitialized($object)) {
+        throw new Exception("Property $property->name is not initialized");
+    } elseif (is_null($property->getValue($object))) {
+        throw new Exception("Property $property->name is null");
+    }
+}
+
+function validateLength(ReflectionProperty $property, object $object, Length $attribute): void
+{
+    if (!$property->isInitialized($object) || is_null($property->getValue($object))) {
+        return; // cancel validation
+    }
+
+    $value = $property->getValue($object);
+    if (strlen($value) < $attribute->min) {
+        throw new Exception("Property $property->name is too short");
+    } elseif (strlen($value) > $attribute->max) {
+        throw new Exception("Property $property->name is too long");
+    }
+}
+
+$loginRequest = new LoginRequest();
+$loginRequest->username = "Reza";
+$loginRequest->password = "reza";
+validate($loginRequest);
