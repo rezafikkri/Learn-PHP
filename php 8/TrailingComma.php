c89bbf2d27c2cfc6f98cdcb09dec54a37ea394bb
rezafikkri <fikkri.reza@gmail.com>
2024-08-01 04:44:44 +0700

    feat: trailing comma

---
+++ php 8/TrailingComma.php
@@ -0,0 +1,23 @@
+<?php
+
+function sum(int ...$number) {
+
+}
+
+sum(
+    1,
+    2,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+);
+
+$array = [
+    'string',
+    23,
+    'IAIN Curup',
+];
