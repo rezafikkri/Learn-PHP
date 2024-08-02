10b58e8242c377d5c0ef9ef414a537a81eb1f6e6
rezafikkri <fikkri.reza@gmail.com>
2024-08-02 08:25:48 +0700

    feat: new string functions

--- php 8/NewStringFunctions.php
+++ php 8/NewStringFunctions.php
@@ -1,5 +1,5 @@
 <?php
 
-var_dump(str_contains("Reza Sariful", "Reza"));
-var_dump(str_starts_with("Reza Sariful", "Reza "));
-var_dump(str_ends_with("Reza Sariful", "Reza "));
+var_dump(str_contains('Reza Sariful Fikri', 'Fikri'));
+var_dump(str_starts_with('Reza Sariful', 'Reza'));
+var_dump(str_ends_with('Reza Sariful', 'Sariful'));
