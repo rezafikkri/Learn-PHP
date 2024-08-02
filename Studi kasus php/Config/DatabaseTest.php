774a6ad28c5868af65cfdbcdc6bd100143200993
rezafikkri <fikkri.reza@gmail.com>
2024-08-03 06:29:55 +0700

    feat(PHP-Dasar-TodoList): Create business logic

--- Studi kasus php/Config/DatabaseTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Config\Database;
-
-require_once __DIR__ . "/Database.php";
-
-$dbh = Database::getConnection();
-echo "Test membuat koneksi ke database.";
---
+++ TodoList(PHP-OOP)/App.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once 'Model/TodoList.php';
+require_once 'BusinessLogic/AddTodoList.php';
+require_once 'BusinessLogic/ShowTodoList.php';
+require_once 'BusinessLogic/RemoveTodoList.php';
