a25c2e8c65ce5f1481e2385fd9a4fb9d9882f3f4
rezafikkri <fikkri.reza@gmail.com>
2024-08-03 17:29:53 +0700

    feat(PHP-Dasar-Todolist): view show todolist

--- TodoList(PHP-OOP)/App.php
+++ TodoList(PHP-OOP)/App.php
@@ -2,7 +2,6 @@
 
 require_once 'Model/TodoList.php';
 require_once 'BusinessLogic/AddTodoList.php';
-require_once 'BusinessLogic/ShowTodoList.php';
 require_once 'BusinessLogic/RemoveTodoList.php';
 require_once 'View/ViewAddTodoList.php';
 require_once 'View/ViewShowTodoList.php';
--- TodoList(PHP-OOP)/View/ViewShowTodoList.php
+++ TodoList(PHP-OOP)/View/ViewShowTodoList.php
@@ -1,6 +1,33 @@
 <?php
 
+require_once '../BusinessLogicShowTodoList.php';
+require_once '../Helper/Input.php';
+
 function viewShowTodoList()
 {
+    while (true) {
+        showTodoList();
 
+        echo "Menu :
+            \t1. Tambah Todo\n
+            \t2. Hapus Todo\n
+            \tx. Keluar" . PHP_EOL;
+
+        $pilihan = input('Pilih');
+
+        switch ($pilihan) {
+            case '1':
+                viewAddTodoList();
+                break;
+            case '2':
+                viewRemoveTodoList();
+                break;
+            case 'x':
+                break 2;
+            default:
+                echo 'Pilihan tidak dimengerti' . PHP_EOL;
+        }
+    }
+
+    echo 'Sampai jumpa lagi' . PHP_EOL;
 }
