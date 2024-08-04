d8d4fca101d381cede7e4ddf2017b9d7b172b290
rezafikkri <fikkri.reza@gmail.com>
2024-08-04 11:43:29 +0700

    feat(PHP-Dasar-Todolist): view remove todolist and test view remove todolist

--- TodoList(PHP-OOP)/View/ViewRemoveTodoList.php
+++ TodoList(PHP-OOP)/View/ViewRemoveTodoList.php
@@ -1,6 +1,19 @@
 <?php
 
+require_once __DIR__ . '/../BusinessLogic/RemoveTodoList.php';
+require_once __DIR__ . '/../Helper/Input.php';
+
 function viewRemoveTodoList()
 {
+    echo 'MENGHAPUS TODOLIST' . PHP_EOL;
 
+    $number = input('Number (x untuk batal)');
+
+    if ($number != 'x') {
+        echo removeTodoList($number) ?
+            "Sukses menghapus todo nomor $number" . PHP_EOL :
+            "Gagal menghapus todo nomor $number" . PHP_EOL;
+    } else {
+        echo 'Batal menghapus todo.' . PHP_EOL;
+    }
 }
