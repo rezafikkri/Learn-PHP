f53e6dc749291956fafb3f2d746aa4a2667fc1b4
rezafikkri <fikkri.reza@gmail.com>
2024-08-04 12:03:31 +0700

    feat: include file

---
+++ php dasar/include/file1.php
@@ -0,0 +1,4 @@
+<?php
+
+$var = 'Reza';
+echo $var;
---
+++ php dasar/include/file2.php
@@ -0,0 +1,4 @@
+<?php
+
+// jika  menggunakan relative path dan di jalankan di terminal, maka path include ataupun require akan relative terhadap posisi folder yang kita buka saat ini diterminal
+include 'file1.php';
