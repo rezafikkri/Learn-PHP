374075935958bcd1f2a23710b542e48523d923c9
rezafikkri <fikkri.reza@gmail.com>
2024-08-04 07:47:56 +0700

    feat(PHP-Dasar-Todolist): test view show todolist

---
+++ TodoList(PHP-OOP)/Test/TestViewShowTodoList.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/../View/ViewShowTodoList.php';
+require_once __DIR__ . '/../BusinessLogic/AddTodoList.php';
+
+addTodoList('Belajar PHP Dasar');
+addTodoList('Belajar PHP OOP');
+addTodoList('Belajar PHP Database');
+
+ViewShowTodoList();
--- TodoList(PHP-OOP)/View/ViewShowTodoList.php
+++ TodoList(PHP-OOP)/View/ViewShowTodoList.php
@@ -1,17 +1,20 @@
 <?php
 
-require_once '../BusinessLogicShowTodoList.php';
-require_once '../Helper/Input.php';
+require_once __DIR__ . '/../Model/TodoList.php';
+require_once __DIR__ . '/../BusinessLogic/ShowTodoList.php';
+require_once __DIR__ . '/../View/ViewAddTodoList.php';
+require_once __DIR__ . '/../View/ViewRemoveTodoList.php';
+require_once __DIR__ . '/../Helper/Input.php';
 
 function viewShowTodoList()
 {
     while (true) {
         showTodoList();
 
-        echo "Menu :
-            \t1. Tambah Todo\n
-            \t2. Hapus Todo\n
-            \tx. Keluar" . PHP_EOL;
+        echo "Menu" . PHP_EOL;
+        echo "1. Tambah Todo" . PHP_EOL;
+        echo "2. Hapus Todo" . PHP_EOL;
+        echo "x. Keluar" . PHP_EOL;
 
         $pilihan = input('Pilih');
 
