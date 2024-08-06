34f3ae93361b80f477d274d1311581a9b747d8e6
rezafikkri <fikkri.reza@gmail.com>
2024-08-06 11:35:57 +0700

    feat(PHP-OOP-TodoList): implement show todoList

--- TodoList(PHP-OOP)/Repository/TodoListRepository.php
+++ TodoList(PHP-OOP)/Repository/TodoListRepository.php
@@ -6,7 +6,27 @@ use PHPOOP\Entity\TodoList;
 
 interface TodoListRepository
 {
+    public function findAll(): array;
     public function save(TodoList $todoList): void;
     public function remove(int $number): bool;
-    public function findAll(): array;
+}
+
+class TodoListRepositoryImpl implements TodoListRepository
+{
+    private $todoLists = [];
+
+    public function findAll(): array
+    {
+        return $this->todoLists;
+    }
+
+    public function save(TodoList $todoList): void
+    {
+        
+    }
+
+    public function remove(int $number): bool
+    {
+        
+    }
 }
--- TodoList(PHP-OOP)/Service/TodoListService.php
+++ TodoList(PHP-OOP)/Service/TodoListService.php
@@ -2,9 +2,39 @@
 
 namespace PHPOOP\Service;
 
+use PHPOOP\Repository\TodoListRepository;
+
 interface TodoListService
 {
     public function showTodoList(): void;
     public function saveTodoList(string $todo): void;
     public function removeTodoList(int $number): bool;
+}
+
+class TodoListServiceImpl implements TodoListService
+{
+    public function __construct (
+        private TodoListRepository $todoListRepository,
+    ) {
+
+    }
+
+    public function showTodoList(): void
+    {
+        echo 'TODOLIST' . PHP_EOL;
+
+        foreach ($this->todoListRepository->findAll() as $number => $value) {
+            echo $number+1 . ". $value" . PHP_EOL;
+        }
+    }
+
+    public function saveTodoList(string $todo): void
+    {
+        
+    }
+
+    public function removeTodoList(int $number): bool
+    {
+        
+    }
 }
