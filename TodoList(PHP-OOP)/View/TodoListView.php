158f1185359184af6a0fcf625af7e6d6ba3a6dba
rezafikkri <fikkri.reza@gmail.com>
2024-08-06 11:17:02 +0700

    feat(PHP-OOP-TodoList): create todo View

---
+++ TodoList(PHP-OOP)/View/TodoListView.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace PHPOOP\View;
+
+class TodoListView
+{
+    public function showTodoList(): void
+    {
+
+    }
+
+    public function addTodoList(): void
+    {
+
+    }
+
+    public function removeTodoList(): void
+    {
+
+    }
+}
