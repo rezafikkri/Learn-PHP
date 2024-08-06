b9c7c09454c96ee82544bd518b7f26c5e5e3e903
rezafikkri <fikkri.reza@gmail.com>
2024-08-06 09:56:26 +0700

    feat(PHP-OOP-TodoList): create todo Repository

---
+++ TodoList(PHP-OOP)/Repository/TodoListRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PHPOOP\Repository;
+
+use PHPOOP\Entity\TodoList;
+
+interface TodoListRepository
+{
+    public function save(TodoList $todoList): void;
+    public function remove(int $number): bool;
+    public function findAll(): array;
+}
