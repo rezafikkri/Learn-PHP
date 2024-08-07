3fdfd95d0d626c51415721abfe81156a47654b3c
rezafikkri <fikkri.reza@gmail.com>
2024-08-07 08:26:44 +0700

    feat(PHP-OOP-TodoList): finish

--- TodoList(PHP-OOP)/App.php
+++ TodoList(PHP-OOP)/App.php
@@ -1,6 +1,15 @@
 <?php
 
-require_once 'Model/TodoList.php';
-require_once 'View/ViewShowTodoList.php';
+require_once __DIR__ . '/Entity/TodoList.php';
+require_once __DIR__ . '/Repository/TodoListRepository.php';
+require_once __DIR__ . '/Service/TodoListService.php';
+require_once __DIR__ . '/Helper/InputHelper.php';
+require_once __DIR__ . '/View/TodoListView.php';
 
-viewShowTodoList();
+use PHPOOP\Repository\TodoListRepositoryImpl;
+use PHPOOP\Service\TodoListServiceImpl;
+use PHPOOP\View\TodoListView;
+
+$todoListRepository = new TodoListRepositoryImpl;
+$todoListService = new TodoListServiceImpl($todoListRepository);
+(new TodoListView($todoListService))->showTodoList();
