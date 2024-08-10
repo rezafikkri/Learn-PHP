1f3cea4e33c0e22d58f9d4506e4a95fd8d326f56
rezafikkri <fikkri.reza@gmail.com>
2024-08-10 19:05:28 +0700

    feat: test query

--- PHP MariaDB Database/testQuery.php
+++ PHP MariaDB Database/testQuery.php
@@ -4,12 +4,16 @@ include 'getConnection.php';
 
 $dbh = getConnection();
 
-$sql = "SELECT * FROM customers";
+$sql = 'SELECT * FROM customers';
 $sth = $dbh->query($sql, PDO::FETCH_ASSOC);
+$sth2 = $dbh->query('SELECT name, email FROM customers', PDO::FETCH_ASSOC);
 
-foreach($sth as $u) {
-    var_dump($u);
+foreach ($sth as $u) {
+    // var_dump($u);
 }
 
+var_dump($sth2->fetchAll());
+
+$sth2 = null;
 $sth = null;
 $dbh = null;
