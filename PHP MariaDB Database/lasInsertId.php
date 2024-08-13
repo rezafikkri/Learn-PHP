cc29e5feec82d57b4438070ee03341cd166a687b
rezafikkri <fikkri.reza@gmail.com>
2024-08-14 06:56:10 +0700

    feat: last insert id

--- PHP MariaDB Database/lasInsertId.php
+++ PHP MariaDB Database/lasInsertId.php
@@ -4,7 +4,7 @@ include __DIR__ . '/getConnection.php';
 
 $dbh = getConnection();
 
-$dbh->exec("INSERT INTO comments (email, comment) VALUES ('Reza@test.com', 'Hay')");
+$dbh->exec("INSERT INTO comment (email, comment) VALUES ('Reza@test.com', 'Hay')");
 $id = $dbh->lastInsertId();
 
-echo $id;
+echo $id . PHP_EOL;
