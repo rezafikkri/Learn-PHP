62bf67b62af2a195d08396ef16facc44d3c43631
rezafikkri <fikkri.reza@gmail.com>
2024-08-30 07:38:21 +0700

    feat: integration with html

--- PHP-Web/index.php
+++ PHP-Web/index.php
@@ -1,3 +1,27 @@
 <?php
+    $title = 'Ini adalah integrasi HTML';
+?>
 
-echo 'HELLO WORLD!';
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title><?= $title ?></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+        input {
+            display: block;
+            margin-bottom: 2px;
+        }
+        </style>
+    </head>
+    <body>
+        <form method="post" action="post.php">
+            <label for="first-name">First name</label>
+            <input type="text" id="first-name" name="first-name"/>
+            <label for="last-name">Last name</label>
+            <input type="text" id="last-name" name="last-name"/>
+            <button type="submit">Sumbit</button>
+        </form>
+    </body>
+</html>
