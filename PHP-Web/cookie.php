44b9ff8faecc4a185e68e1fc1713b2a041cafb1b
rezafikkri <fikkri.reza@gmail.com>
2024-09-04 00:24:11 +0700

    feat: session and cookie

--- PHP-Web/cookie.php
+++ PHP-Web/cookie.php
@@ -1,4 +1,18 @@
 <?php
 
-setcookie("X-BELAJAR-COOKIE", "Reza Sariful Fikri", httponly: true);
+// test expire cookie, jika di server kita set expire-nya kurang dari waktu GMT tapi lebih dari waktu
+// saat ini di local komputer
+// Hasil: kalau di PHP tidak perlu terlalu difikirkan, karena format expire yang diminta adalah unixtimestamp,
+// dan php akan mengkonvertnya secara internal, yaitu dengan timezone GMT, walaupun kita set timezone
+// default server bukan GMT.
+
+// Walaupun dari server timezone nya adalah GMT,
+// tetapi dibrowser akan secara otomatis menyesuaikan, misalnya jika kita set expire dari cookie adalah 1 menit,
+// jadi waktu di GMT misalnya adalah 03 Sep 2024 11:58:00 jadi expire-nya adalah
+// 03 Sep 2024 11:59:00 GMT. Jadi browser akan secara otomatis menyesuaikan, misalnya di browser client
+// timezonenya adalah GMT +07:00, maka expirenya adalah 1 menit dari waktu berdasarkan timezone
+// yang ada dibrowser.
+
+// Jika menggunakan nodejs, karena tidak seperti di PHP yang akan secara internal mengkonvert, maka sebaiknya set expire nya dalam timezone GMT
+setcookie("X-BELAJAR-COOKIE", "Reza Sariful Fikri", time()+60);
 header("location: show-cookie.php");
--- PHP-Web/show-cookie.php
+++ PHP-Web/show-cookie.php
@@ -1,3 +1,4 @@
 <?php
 
 echo "Cookie " . $_COOKIE['X-BELAJAR-COOKIE'];
+var_dump(date_default_timezone_get());
