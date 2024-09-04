783e06ce2f836b646f422c8e7c70179e70662d72
rezafikkri <fikkri.reza@gmail.com>
2024-09-04 17:55:42 +0700

    feat: download

--- PHP-Web/download.php
+++ PHP-Web/download.php
@@ -1,10 +1,10 @@
 <?php
 
 if (isset($_GET['key']) && $_GET['key'] === 'rahasia') {
-    header("Content-Disposition: attachment; filename=profile.jpg");
-    header("Content-Type: image/jpeg");
-    readfile(__DIR__ . "/image/34459deae6c28317d228.jpg");
+    header('Content-Disposition: attachment; filename=profile.jpg');
+    header('Content-Type: image/jpeg');
+    readfile(__DIR__ . '/image/phpEOqoWE.jpg');
     exit;
 } else {
-    echo "Invalid key";
+    echo 'Invalid key';
 }
