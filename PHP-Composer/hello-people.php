415559e461818670995fde0c9ff6ee5aacd40c9b
rezafikkri <fikkri.reza@gmail.com>
2024-09-09 09:43:08 +0700

    feat: composer init

--- PHP-Composer/hello-people.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use RezaFikkri\Data\People;
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-$people = new People('Reza');
-echo $people->sayHello('Adel') . PHP_EOL;
-echo $people->sayHello('Ini adalah reza') . PHP_EOL;
