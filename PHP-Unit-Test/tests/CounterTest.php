0024c159758b32290cbb9c403bf8dceb51e96cd3
rezafikkri <fikkri.reza@gmail.com>
2024-09-15 21:52:35 +0700

    feat(PHP-Unit): Dependency

--- PHP-Unit-Test/tests/CounterTest.php
+++ PHP-Unit-Test/tests/CounterTest.php
@@ -2,6 +2,7 @@
 
 namespace RezaFikkri\PHPUnit;
 
+use PHPUnit\Framework\Attributes\Depends;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
@@ -20,5 +21,22 @@ class CounterTest extends TestCase
 
         $counter->increment();
         $this->assertEquals(3, $counter->getCounter());
+    }
+
+    public function testFirst(): Counter
+    {
+        $counter = new Counter;
+        $counter->increment();
+
+        $this->assertEquals(1, $counter->getCounter());
+
+        return $counter;
+    }
+
+    #[Depends('testFirst')]
+    public function testSecond(Counter $counter): void
+    {
+        $counter->increment();
+        $this->assertEquals(2, $counter->getCounter());
     }
 }
