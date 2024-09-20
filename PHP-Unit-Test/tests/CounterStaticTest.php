d8af1eb4b7ca55ae385cb03961356b5ae255cadb
rezafikkri <fikkri.reza@gmail.com>
2024-09-21 06:22:25 +0700

    feat(PHP-Unit): Sharing fixture

--- PHP-Unit-Test/tests/CounterStaticTest.php
+++ PHP-Unit-Test/tests/CounterStaticTest.php
@@ -9,7 +9,7 @@ class CounterStaticTest extends TestCase
 {
     // Kenapa harus static property? 
     // Karena disetiap test class CounterTest akan selalu di instance, maka jika property bukan static,
-    // value dari property tersebut akan selalu diset ulang (direset). Dan jika property itu static,
+    // value dari property tersebut akan selalu diset ulang (direset). Lain halnya jika property itu static,
     // mau class CounterTest di instance berapa kalipun, tidak akan mempengaruhi nilai dari static property,
     // karena static property adanya di level class bukan level object. Maka sangat cocok untuk membuat
     // sharing fixture.
