de3f12e5593df65283a3d94bca89f226cf4fb2a0
rezafikkri <fikkri.reza@gmail.com>
2024-10-05 18:57:22 +0700

    feat(PHP-MVC): Model

--- PHP-MVC/app/Controller/HomeController.php
+++ PHP-MVC/app/Controller/HomeController.php
@@ -6,11 +6,29 @@ class HomeController
 {
     public function index(): void
     {
+        // model response
+        $response = [
+            'title' => 'Belajar PHP MVC',
+            'content' => 'Selamat Belajar PHP MVC',
+        ];
         echo 'HomeController->index()';
     }
 
     public function hello(): void
     {
         echo 'HomeController->hello()';
+    }
+
+    public function login(): void
+    {
+        // model request
+        $request = [
+            'username' => htmlspecialchars($_POST['username']),
+            'password' => htmlspecialchars($_POST['password']),
+        ];
+
+        $response = [
+            'message' => 'Login success',
+        ];
     }
 }
