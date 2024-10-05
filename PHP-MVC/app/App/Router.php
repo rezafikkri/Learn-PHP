6572b1d729e4bbb01856d4b8665e9a84af919a14
rezafikkri <fikkri.reza@gmail.com>
2024-10-05 21:02:01 +0700

    feat(PHP-MVC): Middleware

--- PHP-MVC/app/App/Router.php
+++ PHP-MVC/app/App/Router.php
@@ -2,6 +2,9 @@
 
 namespace RezaFikkri\MVC\App;
 
+use Exception;
+use RezaFikkri\MVC\Middleware\Middleware;
+
 class Router
 {
     private static array $routes = [];
@@ -11,12 +14,14 @@ class Router
         string $path,
         string $controller,
         string $method,
+        array $middleware = [],
     ): void {
         self::$routes[] = [
             'httpMethod' => $httpMethod,
             'path' => $path,
             'controller' => $controller,
             'method' => $method,
+            'middleware' => $middleware,
         ];
     }
 
@@ -28,9 +33,19 @@ class Router
 
         foreach (self::$routes as $route) {
             $pattern = "#^$route[path]$#";
+
             if (preg_match($pattern, $path, $variables) && $httpMethod == $route['httpMethod']) {
+                foreach ($route['middleware'] as $middleware) {
+                    // run middleware
+                    $middlewareObj = new $middleware;
+                    if (!$middlewareObj instanceof Middleware) {
+                        throw new Exception("Middleware $middleware must be implement Middleware interface!");
+                    }
+                    $middlewareObj->before();
+                }
+
+                // run controller
                 $controller = new $route['controller'];
-                // $controller->{$route['method']}();
                 array_shift($variables);
                 call_user_func_array([$controller, $route['method']], $variables);
                 return;
