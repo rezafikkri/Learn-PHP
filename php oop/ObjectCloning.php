9f47d3f7509af30487444528fbdc0df37202a51b
rezafikkri <fikkri.reza@gmail.com>
2024-10-12 19:15:33 +0700

    feat: add test for modify object property in function

--- php oop/ObjectCloning.php
+++ php oop/ObjectCloning.php
@@ -10,7 +10,13 @@ $student1->age = 22;
 $student1->value = 100;
 $student1->setSample("XXX");
 
-$student2 = clone $student1;
+$student2 = $student1;
+
+function testModifyObjectProperty(Student $student) {
+    $student->name = 'Modified';
+}
+
+testModifyObjectProperty($student2);
 
 var_dump($student1);
-var_dump($student2);
+var_dump($student1 === $student2);
