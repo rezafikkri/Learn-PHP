eccdd66dac3821fc360650a8e969146fc31dca3d
rezafikkri <fikkri.reza@gmail.com>
2024-11-17 09:26:05 +0700

    feat(PHP8.1): Enumerations Method

---
+++ php 8.1/EnumerationsMethod.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace PHP81\EnumerationMethod;
+
+use Error;
+
+enum Gender: string
+{
+    case Male = 'Mr';
+    case Female = 'Mrs';
+
+    public function inIndonesia(): string
+    {
+        return match($this) {
+            Gender::Male => 'Tuan',
+            Gender::Female => 'Nyonya',
+        };
+    }
+
+    public static function fromIndonesia(string $value): Gender
+    {
+        return match($value) {
+            'Tuan' => Gender::Male,
+            'Nyonya' => Gender::Female,
+            default => throw new Error('Unsopported Indonesian Value'),
+        };
+    }
+}
+
+class Customer
+{
+    public function __construct(
+        public string $name,
+        public Gender $gender,
+    ) {
+        
+    }
+}
+
+function sayHello(Customer $customer): string
+{
+    return "Hello {$customer->gender->inIndonesia()}.$customer->name" . PHP_EOL;
+}
+
+echo sayHello(new Customer('Reza', Gender::Male));
+echo sayHello(new Customer('Adelina', Gender::from('Mrs')));
+
+var_dump(Gender::fromIndonesia('Tuan'));
